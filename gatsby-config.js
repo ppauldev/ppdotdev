da889179c1641085ef58b0c4b81ebf66ffaf345f
Phillip Paul <phillippaul@gmx.de>
2021-09-12 19:31:21 +0200

    Add tsconfig, blank index

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.phillippaul.dev",
     title: "ppdotdev",
   },
   plugins: [
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <div>Hello world! </div>
+  )
+}
+
+export default IndexPage
