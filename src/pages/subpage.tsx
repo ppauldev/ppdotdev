2895d2d946a008ef9cfa9f0322196cfa0f18526a
Phillip Paul <phillippaul@gmx.de>
2021-09-13 01:01:33 +0200

    Initial test graphql

--- src/pages/subpage.tsx
+++ src/pages/subpage.tsx
@@ -1,18 +1,43 @@
 import React, { FC, ReactElement } from 'react'
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client"
 
 type SubpageProps = {
-    name: string,
-    id: number,
-    bio?: string
+  name: string,
+  id: number,
+  bio?: string
 }
 
+const client = new ApolloClient({
+  uri: 'https://api-eu-central-1.graphcms.com/v2/ckthkvaiu44rx01xq5ffr9yrz/master',
+  cache: new InMemoryCache()
+});
+
 const subpage: FC<SubpageProps> = ({ name = "Paul", id = 3, bio = 'Bio empty' }): ReactElement => {
-    return (
-        <div>
-            <h2>Name: {name}, Id: {id}</h2>
-            <p>{bio}</p>
-        </div>
-    )
+  React.useEffect(() => {
+    client
+      .query({
+        query: gql`
+        { 
+          posts {
+            id
+            title
+            slug
+            date
+            preview
+            body
+          }
+        }
+      `
+      })
+      .then(result => console.log(result));
+  }, [])
+
+  return (
+    <div>
+      <h2>Name: {name}, Id: {id}</h2>
+      <p>{bio}</p>
+    </div>
+  )
 }
 
 export default subpage
