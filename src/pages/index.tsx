86754fae1ca66b6182dc1a04a43b43e57cfce766
Phillip Paul <phillippaul@gmx.de>
2021-09-18 03:14:03 +0200

    Reordering file structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import { GRAPH_CMS_API_URL } from "../api/apiConfig"
-import Webview from "./webview"
+import Webview from "../webview"
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
