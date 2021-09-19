ae60314f9640c77bea38da1be64478944b5ba3ad
Phillip Paul <phillippaul@gmx.de>
2021-09-19 03:41:07 +0200

    Add footer info

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,39 @@
 import * as React from "react"
 
+import { LogoGithub, LogoLinkedin, Mail } from 'react-ionicons'
+
+import "./footer.css"
+
 const Footer: React.FC = (): React.ReactElement => {
   return (
-    <footer>Footer area</footer>
+    <footer>
+      <div>
+        <div>© 2021 Phillip Paul</div>
+        <div id="footer-break">|</div>
+        <div>
+          <LogoGithub
+            className={"social-icon"}
+            color={'#00000'}
+            height="22px"
+            width="22px"
+          />
+          <LogoLinkedin
+            className={"social-icon"}
+            color={'#00000'}
+            height="22px"
+            width="22px"
+          />
+          <Mail
+            className={"social-icon"}
+            color={'#00000'}
+            height="22px"
+            width="22px"
+          />
+        </div>
+        <div id="footer-break">|</div>
+        <div>Imprint & Data protection</div>
+      </div>
+    </footer>
   )
 }
 
