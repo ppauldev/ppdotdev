eea617215ef5181a414ab952495a64520ff90dfc
Phillip Paul <phillippaul@gmx.de>
2021-12-15 19:28:54 +0100

    Add more tech tags and add URLs

--- src/helper/techTagInfo.tsx
+++ src/helper/techTagInfo.tsx
@@ -1,8 +1,50 @@
 export const techTagInfo: { [index: string]: any } = {
+  css3: {
+    url: "https://www.w3.org/Style/CSS/",
+  },
+  electron: {
+    url: "https://www.electronjs.org/",
+  },
+  git: {
+    url: "https://www.git-scm.com/",
+  },
+  github: {
+    url: "https://www.github.com/",
+  },
+  heroku: {
+    url: "https://www.heroku.com/",
+  },
+  html5: {
+    url: "https://html.spec.whatwg.org/multipage/",
+  },
   javascript: {
     url: "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/",
   },
+  mongodb: {
+    url: "https://www.mongodb.com/",
+  },
+  nodejs: {
+    url: "https://www.nodejs.org/",
+  },
+  npm: {
+    url: "https://www.npmjs.com/",
+  },
+  postgresql: {
+    url: "https://www.postgresql.org/",
+  },
+  python: {
+    url: "https://www.python.org/",
+  },
+  react: {
+    url: "https://www.reactjs.org/",
+  },
+  redis: {
+    url: "https://www.redis.io/",
+  },
   typescript: {
     url: "https://www.typescriptlang.org/",
+  },
+  vuejs: {
+    url: "https://www.vuejs.org/",
   },
 }
