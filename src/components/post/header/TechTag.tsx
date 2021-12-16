0856110f96b79440d3baf9d262981b08674eb18e
Phillip Paul <phillippaul@gmx.de>
2021-12-16 23:01:58 +0100

    Extend footer, add icons

--- src/components/post/header/TechTag.tsx
+++ src/components/post/header/TechTag.tsx
@@ -12,9 +12,10 @@ const TechTag: React.FC<ITechTag> = ({ keyword }): React.ReactElement => {
   const normalizedKeyword = keyword.toLowerCase()
   const techTagClassName = `tech-tag ${normalizedKeyword}`
   const techTagUrl = techTagInfo[normalizedKeyword]?.url
+  const techTagSource = techTagInfo[normalizedKeyword]?.imageSource
 
   return (
-    <a className={techTagClassName} href={techTagUrl} />
+    <a className={techTagClassName} href={techTagUrl} data-imagesource={techTagSource} />
   )
 }
 
