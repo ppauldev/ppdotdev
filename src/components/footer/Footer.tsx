50370db0de5e9ca83996b7f64cd1d9bf1cfd0762
Phillip Paul <phillippaul@gmx.de>
2021-12-17 11:39:54 +0100

    Adjust footer items on mobile screens

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,22 +8,26 @@ const Footer: React.FC = (): React.ReactElement => {
   return (
     <footer>
       <div>
-        <div className="footer-item-wrapper">
+        <div className="footer-item-wrapper author">
           <p>© 2021 Phillip Paul</p>
         </div>
         <div id="footer-break">|</div>
         <div className="footer-item-wrapper">
           <p>contact:</p>
-          <TechTag key="github" keyword="github" />
-          <TechTag key="linkedin" keyword="linkedin" />
-          <TechTag key="mail" keyword="mail" />
+          <div className="tech-tags">
+            <TechTag key="github" keyword="github" />
+            <TechTag key="linkedin" keyword="linkedin" />
+            <TechTag key="mail" keyword="mail" />
+          </div>
         </div>
         <div id="footer-break">|</div>
         <div className="footer-item-wrapper">
           <p>powered by:</p>
-          <TechTag key="react" keyword="react" />
-          <TechTag key="typescript" keyword="typescript" />
-          <TechTag key="gatsby" keyword="gatsby" />
+          <div className="tech-tags">
+            <TechTag key="react" keyword="react" />
+            <TechTag key="typescript" keyword="typescript" />
+            <TechTag key="gatsby" keyword="gatsby" />
+          </div>
         </div>
       </div>
     </footer>
