8aadeae8cdd65f390c7939b9579277194b0a4a64
Phillip Paul <phillippaul@gmx.de>
2021-12-19 00:33:46 +0100

    Add responsive contact form

--- src/components/main/EmailForm.tsx
+++ src/components/main/EmailForm.tsx
@@ -1,25 +1,47 @@
 import * as React from "react"
 
+import "./email_form.css"
+
 const EmailForm: React.FC = (): React.ReactElement => {
   return (
     <main>
-      <section>
-        <form method="post" action="https://go.phillippaul.dev/mail">
-          <label>
-            Email
-            <input type="email" name="email" />
-          </label>
-          <label>
-            Name
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Message
-            <input type="text" name="message" />
-          </label>
-          <input type="submit" value="send" />
-        </form>
-      </section>
+      <div id="email-section">
+        <div id="email-form-wrapper">
+          <form method="post" action="https://go.phillippaul.dev/mail">
+            <div className="top-contact-area">
+              <h3>Contact</h3>
+            </div>
+            <div className="middle-contact-area">
+              <div className="email-area">
+                <p>Email</p>
+                <input type="email" name="email" placeholder="Your email" required />
+              </div>
+              <div className="name-area">
+                <p>Name</p>
+                <input type="text" name="name" placeholder="Your name" required />
+              </div>
+            </div>
+            <div className="topic-area">
+              <p>Topic</p>
+              <select required>
+                <option value="" disabled selected>Please select ..</option>
+                <option value="general-question">General question</option>
+                <option value="content">Content request</option>
+                <option value="project">Project inquiry</option>
+                <option value="bug">Found a bug!</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="message-area">
+              <p>Message</p>
+              <input type="text" name="message" required />
+            </div>
+            <div className="button-area">
+              <input type="submit" value="Send" />
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   )
 }
