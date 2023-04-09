256efcf1adc68315c305915fa70bbb767b671a52
Phillip Paul <ppaul_dev@t-online.de>
2023-04-09 19:38:06 +0200

    Add image hover zoom effect

--- src/components/main/PostTile.tsx
+++ src/components/main/PostTile.tsx
@@ -20,15 +20,35 @@ interface IPostTileProps {
 
 const PostTile: React.FC<IPostTile> = ({ post }): React.ReactElement => {
   return (
-    <a href={post.slug} className="post-tile-slug">
+    <a
+      href={post.slug}
+      className="post-tile-slug"
+      onMouseOver={() => {
+        const img = document.getElementById(`post-tile-image-${post.slug}`)
+        if (img) {
+          img.style.transform = "scale(1.1)"
+        }
+      }}
+      onMouseLeave={() => {
+        const img = document.getElementById(`post-tile-image-${post.slug}`)
+        if (img) {
+          img.style.transform = "scale(1)"
+        }
+      }}
+    >
       <article className="post-tile">
+        <div className="post-tile-image-wrapper">
+          <div
+            id={`post-tile-image-${post.slug}`}
+            className="post-tile-image"
+            style={{ background: `url(${post?.image?.url}) 50% / 100%` }}
+            data-imagesource={post?.imageSource}
+            data-imagelicense={post?.imageLicense}
+          />
+        </div>
         <div
-          className="post-tile-image"
-          style={{ background: `url(${post?.image?.url}) 50% / 100%` }}
-          data-imagesource={post?.imageSource}
-          data-imagelicense={post?.imageLicense}
-        />
-        <div className="post-tile-content">
+          className="post-tile-content"
+        >
           <div className="post-tile-content-main">
             <h2>{post.title}</h2>
             <h3>{post.preview}</h3>
