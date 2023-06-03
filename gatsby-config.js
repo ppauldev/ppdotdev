fe5c56f5d54b2be66c7db9197828fd00129c9939
Phillip Paul <ppaul_dev@t-online.de>
2023-06-03 23:27:14 +0200

    Remove google gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,24 +25,6 @@ module.exports = {
       options: {
         icon: "src/assets/ppdotdev_icon.svg",
       },
-    },
-    {
-      resolve: "gatsby-plugin-google-gtag",
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          process.env.GATSBY_GOOGLE_GTAG, // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
-    },
+    }
   ],
 }
